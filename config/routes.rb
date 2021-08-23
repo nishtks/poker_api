8821fd699f1cd92e493f009d370a486388f89d6d
nishtks <nish.tks@gamil.com>
2021-08-23 11:17:10 +0900

    apiを追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'cards/top' => 'cards#top'
-  post 'cards/submit' => 'cards#submit' 
-  get 'cards/judge' => 'cards#judge' 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #  get 'cards/top' => 'cards#top'
+  #  post 'cards/submit' => 'cards#submit'
+  #  get 'cards/judge' => 'cards#judge'
+
+mount Base::API => '/'  # api/base/api　の上に居る
 end
